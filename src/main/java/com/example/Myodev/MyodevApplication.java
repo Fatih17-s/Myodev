e147073277306dd673fbe95fceb1c309a8a359aa
fsuna5663@gmail.com <fsuna5663@gmail.com>
2024-08-19 13:48:51 +0300

    update

---
+++ src/main/java/com/example/Myodev/MyodevApplication.java
@@ -0,0 +1,13 @@
+package com.example.Myodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyodevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Myodev/MyodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Myodev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyodevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
